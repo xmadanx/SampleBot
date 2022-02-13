fed23e4e60c4530b088af57d5bfa2a626e439db8
“Madan” <madanmn1742@gmaill.com>
2022-02-13 22:22:32 +0530

    Adding main

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-//start
-print("Hello World")
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+updater = Updater('YOUR TOKEN HERE')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
