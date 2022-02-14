1a5879fee8d4f9bce5a2d3496fb04c6350d79637
“Madan” <madanmn1742@gmaill.com>
2022-02-14 20:33:03 +0530

    Final

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-updater = Updater('5155754757:AAGHPpySj_7Zx1qmZD2eY9jTW98Co6fESNo')
+updater = Updater('5245387008:AAGLGmulUKVsRhcRKgtIubuH9Q8w7aknpEU')
 
 updater.dispatcher.add_handler(CommandHandler('start', hello))
 
