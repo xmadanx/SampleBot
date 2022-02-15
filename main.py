df3851f0f8ab4ad3ac452686b17e2f24c6c05887
“Madan” <madanmn1742@gmaill.com>
2022-02-15 12:13:57 +0530

    Done

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
-    update.message.reply_text(f'I am LoveQueenBot and I am here to Share Love <3')
+    update.message.reply_text(f'I am Madan\'s LoveQueenBot and I am here to Share Love <3')
+    update.message.reply_text(f'Love You {update.effective_user.first_name}')
+    update.message.reply_text(f'Click this /text to text Madan')
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Mmm..Text to @madan1742')
+def text(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Mmm.. Text to @madan1742')
 
 updater = Updater('5245387008:AAGLGmulUKVsRhcRKgtIubuH9Q8w7aknpEU')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('text', text))
 
 updater.start_polling()
 updater.idle()
